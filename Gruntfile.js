4a5991e369c883a9f989ffd1dce69da5e10ecf40
Bartvds <bartvanderschoor@gmail.com>
2013-06-12 23:15:03 +0200

    testing setup: json schemas, mocha/chai, empty unit tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,13 +9,16 @@ module.exports = function (grunt){
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-execute');
     grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-json-schema');
 
     grunt.loadTasks('tasks');
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         clean: {
-            build: ['tmp', 'deploy/*.js', 'deploy/*.json', 'tools/build.js', 'DefinitelyTyped'],
+            tmp : ['tmp', 'test/**/_tmp.*.*'],
+            build: ['deploy/*.js', 'deploy/*.json', 'tools/build.js', 'DefinitelyTyped'],
             repo: ['repo/*.json']
         },
         copy: {
@@ -46,6 +49,16 @@ module.exports = function (grunt){
             api: {
                 src: ['src/api.ts'],
                 dest: 'deploy/api.js'
+            },
+            test_node: {
+                option: {base_path: 'test/node/'},
+                src: ['test/node/*.test.ts'],
+                dest: 'test/node/_tmp.test.js'
+            },
+            test_repo: {
+                option: {base_path: 'test/repo/'},
+                src: ['test/repo/*.test.ts'],
+                dest: 'test/repo/_tmp.test.js'
             }
         },
         build_repo: {
@@ -55,20 +68,54 @@ module.exports = function (grunt){
             all: {
                 src: ['repo_data/*.json']
             }
+        },
+        json_schema: {
+            repo_data: {
+                'files' : {
+                    'schema/repo_data.json': ['repo_data/*.json']
+                }
+            },
+            repo_v2: {
+                'files' : {
+                    'schema/repository_v2.json': ['repo/repository_v2.json']
+                }
+            },
+            repo_site: {
+                'files' : {
+                    'schema/repository_site.json': ['repo/repository_site.json']
+                }
+            }
+        },
+        mochaTest: {
+            any: {
+                src: ['test/**/*.test.js'],
+                options: {
+                    reporter: 'mocha-unfunk-reporter'
+                }
+            }
         }
     });
 
     grunt.registerTask('compile-cli', ['typescript:cli']);
     grunt.registerTask('compile-api', ['typescript:api']);
 
-    grunt.registerTask('git-DefinitelyTyped-clone', ['shell:gitDefinitelyTypedClone']);
+    grunt.registerTask('validate-data', ['json_schema:repo_data']);
+    grunt.registerTask('validate-repo', ['typescript:test_repo', 'mochaTest', 'json_schema:repo_v2', 'json_schema:repo_site']);
 
-    grunt.registerTask('build', ['clean:build', 'compile-api', 'compile-cli']);
+    grunt.registerTask('test-code', ['typescript:test_node', 'mochaTest']);
+
+    grunt.registerTask('build', ['clean:tmp', 'clean:build', 'compile-api', 'compile-cli', 'test-code']);
+    grunt.registerTask('repo', ['clean:tmp', 'clean:repo','test-code', 'validate-data', 'build', 'build_repo:all', 'validate-repo', 'copy:export_repo']);
 
     // cli commands
     grunt.registerTask('default', ['build']);
-    grunt.registerTask('repo', ['default', 'clean:repo', 'build_repo:all', 'copy:export_repo']);
+    grunt.registerTask('test', ['test-code', 'validate-data', 'validate-repo']);
 
     // additional editor toolbar mappings
-    grunt.registerTask('dev', ['repo']);
+    grunt.registerTask('dev', []);
+    grunt.registerTask('edit_01', ['validate-data']);
+    grunt.registerTask('edit_02', ['validate-repo']);
+    grunt.registerTask('edit_03', ['repo']);
+    grunt.registerTask('edit_04', []);
+    grunt.registerTask('edit_05', []);
 };
