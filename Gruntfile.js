cc84ed1f402f6eed49374843a44ee659ad369bd5
Bartvds <bartvanderschoor@gmail.com>
2013-06-12 15:03:19 +0200

    repo json building to grunt, moved repo json to it's own folder, ditched tsd-libs fragments

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,65 +1,74 @@
-module.exports = function (grunt) {
-	'use strict';
+module.exports = function (grunt){
+    'use strict';
 
-	var path = require('path');
-	var util = require('util');
+    var path = require('path');
+    var util = require('util');
 
-	grunt.loadNpmTasks('grunt-typescript');
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-execute');
-	grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-typescript');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-execute');
+    grunt.loadNpmTasks('grunt-shell');
 
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		clean: {
-			build : ['deploy/*.js', 'deploy/*.json', 'tools/build.js', 'DefinitelyTyped']
-		},
-		shell: {
-		    gitDefinitelyTypedClone: {
-		        command: 'git clone git@github.com:borisyankov/DefinitelyTyped.git'
-		    }
-		},
-		typescript: {
-			options: {
-				module: 'commonjs',
-				target: 'es5',
-				base_path: 'src/',
-				declaration: false,
-				sourcemap: false
-			},
-			cli: {
-				src: ['src/commandLine.ts'],
-				dest: 'deploy/commandLine.js'
-			},
-			api: {
-				src: ['src/api.ts'],
-				dest: 'deploy/api.js'
-			},
-			repo: {
-				src: ['tools/buildRepository.ts'],
-				dest: 'tools/build.js'
-			}
-		},
-		execute: {
-			repo: {
-				src: ['tools/build.js']
-			}
-		}
-	});
+    grunt.loadTasks('tasks');
 
-	grunt.registerTask('compile-cli', ['typescript:cli']);
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        clean: {
+            build: ['tmp', 'deploy/*.js', 'deploy/*.json', 'tools/build.js', 'DefinitelyTyped'],
+            repo: ['repo/*.json']
+        },
+        copy: {
+            export_repo: {
+                files: [
+                    {src: 'repo/repository.js', dest: './../tsdpm-site/tmpl/'},
+                    {src: 'repo/repository_v2.json', dest: './../tsdpm-site/'}
+                ]
+            }
+        },
+        shell: {
+            gitDefinitelyTypedClone: {
+                command: 'git clone git://github.com/borisyankov/DefinitelyTyped.git'
+            }
+        },
+        typescript: {
+            options: {
+                module: 'commonjs',
+                target: 'es5',
+                base_path: 'src/',
+                declaration: false,
+                sourcemap: false
+            },
+            cli: {
+                src: ['src/commandLine.ts'],
+                dest: 'deploy/commandLine.js'
+            },
+            api: {
+                src: ['src/api.ts'],
+                dest: 'deploy/api.js'
+            }
+        },
+        build_repo: {
+            options: {
+                pretty: 2
+            },
+            all: {
+                src: ['repo_data/*.json']
+            }
+        }
+    });
 
-	grunt.registerTask('compile-api', ['typescript:api']);
+    grunt.registerTask('compile-cli', ['typescript:cli']);
+    grunt.registerTask('compile-api', ['typescript:api']);
 
-	grunt.registerTask('compile-repo', ['typescript:repo']);
+    grunt.registerTask('git-DefinitelyTyped-clone', ['shell:gitDefinitelyTypedClone']);
 
-	grunt.registerTask('exec-repo', ['execute:repo']);
+    grunt.registerTask('build', ['clean:build', 'compile-api', 'compile-cli']);
 
-	grunt.registerTask('git-DefinitelyTyped-clone', ['shell:gitDefinitelyTypedClone']);
+    // cli commands
+    grunt.registerTask('default', ['build']);
+    grunt.registerTask('repo', ['default', 'clean:repo', 'build_repo:all', 'copy:export_repo']);
 
-	// cli commands
-	grunt.registerTask('default', ['clean', 'compile-api', 'compile-cli']);
-
-	grunt.registerTask('repo', ['default', 'compile-repo', 'git-DefinitelyTyped-clone', 'exec-repo']);	
+    // additional editor toolbar mappings
+    grunt.registerTask('dev', ['repo']);
 };
