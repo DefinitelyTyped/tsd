37b337a1651aeb16bd248b87934e3e3993f1e30c
Bart van der Schoor <bartvanderschoor@gmail.com>
2014-03-30 03:09:26 +0200

    Updates

--- js/index.js
+++ js/index.js
@@ -1780,7 +1780,10 @@ var path = require('path');
 
 exports.absoluteURI = function absoluteURI(rel) {
 	var u = urlMod.parse(window.location.href);
-	u.pathname = path.dirname(u.pathname) + '/' + rel;
+	if (!/\/$/.test(u.pathname)) {
+		u.pathname = path.dirname(u.pathname);
+	}
+	u.pathname = u.pathname.replace(/\/?$/,'/') + rel;
 	delete u.search;
 	delete u.query;
 	delete u.hash;
