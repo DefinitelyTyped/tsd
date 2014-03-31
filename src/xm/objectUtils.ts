3912443050c2b0307bc88e9400af88ce97417f76
Bart van der Schoor <bartvanderschoor@gmail.com>
2014-03-31 21:09:37 +0200

    restructured objectUtils methods

--- src/xm/objectUtils.ts
+++ src/xm/objectUtils.ts
@@ -1,5 +1,7 @@
 /// <reference path="./_ref.d.ts" />
 
+'use strict';
+
 import typeOf = require('./typeOf');
 
 // TODO remove some method clutter
@@ -21,6 +23,7 @@ function deepFreezeRecursive(object: any, active: any[]): void {
 	}
 }
 
+// just here for consistency
 export function defineProp(object: Object, property: string, settings: any): void {
 	Object.defineProperty(object, property, settings);
 }
@@ -31,23 +34,34 @@ export function defineProps(object: Object, propertyNames: string[], settings: a
 	});
 }
 
-export function hidePrefixed(object: Object, ownOnly: boolean = true): void {
-	for (var property in object) {
-		if (property.charAt(0) === '_' && (!ownOnly || typeOf.hasOwnProp(object, property))) {
-			defineProp(object, property, {enumerable: false});
-		}
+export function hideProps(object: Object, props?: string[], pub: boolean = true, pref: boolean = true, own: boolean = true) {
+	if (!props) {
+		props = Object.keys(object);
 	}
-}
-
-export function hideProps(object: Object, props: string[]) {
 	props.forEach((property: string) => {
-		Object.defineProperty(object, property, {enumerable: false});
+		if (own && !typeOf.hasOwnProp(object, property)) {
+			return;
+		}
+		if (property.charAt(0) === '_') {
+			if (pref) {
+				Object.defineProperty(object, property, {enumerable: false});
+			}
+		}
+		else if (pub) {
+			Object.defineProperty(object, property, {enumerable: false});
+		}
 	});
 }
 
-export function lockProps(object: Object, props: string[], pub: boolean = true, pref: boolean = true) {
+export function lockProps(object: Object, props?: string[], pub: boolean = true, pref: boolean = true, own: boolean = true) {
+	if (!props) {
+		props = Object.keys(object);
+	}
 	props.forEach((property: string) => {
-		if (/^_/.test(property)) {
+		if (own && !typeOf.hasOwnProp(object, property)) {
+			return;
+		}
+		if (property.charAt(0) === '_') {
 			if (pref) {
 				Object.defineProperty(object, property, {writable: false});
 			}
@@ -58,16 +72,30 @@ export function lockProps(object: Object, props: string[], pub: boolean = true, 
 	});
 }
 
-export function forceProps(object: Object, props: Object) {
-	Object.keys(props).forEach((property: string) => {
-		Object.defineProperty(object, property, {value: props[property], writable: false});
+export function freezeProps(object: Object, props?: string[], pub: boolean = true, pref: boolean = true, own: boolean = true) {
+	if (!props) {
+		props = Object.keys(object);
+	}
+	props.forEach((property: string) => {
+		if (own && !typeOf.hasOwnProp(object, property)) {
+			return;
+		}
+		if (property.charAt(0) === '_') {
+			if (pref) {
+				Object.defineProperty(object, property, {writable: false});
+				Object.freeze(object[property]);
+			}
+		}
+		else if (pub) {
+			Object.defineProperty(object, property, {writable: false});
+			Object.freeze(object[property]);
+		}
 	});
 }
 
-export function freezeProps(object: Object, props: string[]) {
-	props.forEach((property: string) => {
-		Object.defineProperty(object, property, {writable: false});
-		Object.freeze(object[property]);
+export function forceProps(object: Object, props: Object) {
+	Object.keys(props).forEach((property: string) => {
+		Object.defineProperty(object, property, {value: props[property], writable: false});
 	});
 }
 
