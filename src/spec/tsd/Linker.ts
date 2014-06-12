1f4f4fa3ea39c2b6a96614c6f0a9bab8be5bf962
Bart van der Schoor <bartvanderschoor@gmail.com>
2014-06-12 04:45:28 +0200

    re-enable all tests
    i am a silly person

--- src/spec/tsd/Linker.ts
+++ src/spec/tsd/Linker.ts
@@ -14,7 +14,7 @@ import helper = require('../../test/helper');
 import PackageLinker = require('../../tsd/support/PackageLinker');
 import PackageDefinition = require('../../tsd/support/PackageDefinition');
 
-describe.only('PackageLinker', () => {
+describe('PackageLinker', () => {
 
 	var linkerFixtures = path.join(helper.getSharedFixtures(), 'linker');
 
