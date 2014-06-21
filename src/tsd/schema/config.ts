ee07e12c061e6b8d7bca14b158d962f43ac38cdd
Bart van der Schoor <bartvanderschoor@gmail.com>
2014-06-21 10:27:09 +0200

    fixed schema path case-sensitivity

--- src/tsd/schema/config.ts
+++ src/tsd/schema/config.ts
@@ -42,7 +42,7 @@ var schema = Joi.object({
 		.description('toggle stats tracking'),
 	installed: Joi
 		.object()
-		.pattern(/^([a-z](?:[\._-]?[a-z0-9]+)*)(\/[a-z](?:[\._-]?[a-z0-9]+)*)+\.d\.ts$/, Joi.object({
+		.pattern(/^([a-z](?:[\._-]?[a-z0-9]+)*)(\/[a-z](?:[\._-]?[a-z0-9]+)*)+\.d\.ts$/i, Joi.object({
 			commit: Joi.string()
 				.required()
 				.regex(/^[0-9a-f]{6,40}$/)
